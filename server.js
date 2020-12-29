acc9e1262e4a55d6b1d626f35556cd173732abf1
Nickolas Menotti <nmenotti@cox.net>
2020-12-29 11:29:22 -0800

    require mysql, and inquirer and establish a connection to localhost

---
+++ server.js
@@ -0,0 +1,30 @@
+// Require mysql, inquirer and console.table modules //
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// var consoleTable = require("console.table");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "Employee_MS_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+
+
